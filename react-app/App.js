08c2f365fa8e0ed070c3bc5d065356771083da43
karenzhao35 <karenzhao35@gmail.com>
2024-04-06 13:29:39 -0700

    added .env configuration

--- react-app/App.js
+++ react-app/App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+require('dotenv').config()
 
 export default function App() {
   return (
--- react-app/babel.config.js
+++ react-app/babel.config.js
@@ -1,5 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
+  plugins: [
+    ['module:react-native-dotenv']
+  ]
   return {
     presets: ['babel-preset-expo'],
   };
